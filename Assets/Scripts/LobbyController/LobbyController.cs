e94909802058a95bd67f7c0da50ba150356ed8c7
Kathiravan Thirumurthy <tkathiravan1976@gmail.com>
2022-03-12 11:33:26 +0530

    Lobby LevelSelector and Level1 setup

---
+++ Assets/Scripts/LobbyController/LobbyController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyController : MonoBehaviour
+{
+    //Getting the Play button
+    [SerializeField]
+    private Button _startBtn;
+    //Gettig the Quit Button
+    [SerializeField]
+    private Button _quitBtn;
+    //The Level selector game object to hide and show
+    [SerializeField]
+    private GameObject _levelSelector;
+    private void Awake()
+    {
+        // adding the listener to the buttons
+        _startBtn.onClick.AddListener(LevelSelect);
+        _quitBtn.onClick.AddListener(QuitGame);
+    }
+    //function to load the select level
+    private void LevelSelect()
+    {
+        // levelSelector Gameobject is enabled
+
+        _levelSelector.SetActive(true);
+    }
+    //function to quit the game
+    private void QuitGame()
+    {
+        //Quitting the Application i.e Game
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/SceneController/Scenecontroller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+///  To load different levels in the game
+/// </summary>
+public class Scenecontroller : MonoBehaviour
+{
+    // Initialising Level button 
+    private Button _levelButton;
+    //levelName should be addded to move for the particular level 
+    [SerializeField]
+    private string levelName;
+
+    void Awake()
+    {
+        //Getting the component of the Button GameObject
+        _levelButton = GetComponent<Button>();
+        // adding listener to the level Buttons
+        _levelButton.onClick.AddListener(onClick);
+    }
+    // function to load scene with given string
+    private void onClick()
+    {
+        SceneManager.LoadScene(levelName);
+        Debug.Log(levelName);
+    }
+}
